7ecffd74033a765d1856ad433dece98811f125eb
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-10-18 16:26:09 +0900

    skeleton

---
+++ src/routers/postingsRouter.js
@@ -0,0 +1,22 @@
+// import express from "express";
+// import {
+//   postPostings,
+//   patchPostings,
+//   deletePostings,
+//   readAllPostings,
+//   postComment,
+//   getComments,
+//   deleteComment,
+//   patchComment,
+// } from "../controller/postingController.js";
+// import { authMiddleware } from "../middlewares/authMiddleware.js";
+// const postingRouter = express.Router();
+
+// postingRouter
+//   .route("/:id/comments")
+//   .post(authMiddleware, postComment)
+//   .get(authMiddleware, getComments)
+//   .delete(authMiddleware, deleteComment)
+//   .patch(authMiddleware, patchComment);
+
+// export default postingRouter;
---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+import renderRouter from "./routers/renderRouter.js";
+import postingsRouter from "./routers/postingsRouter.js";
+import usersRouter from "./routers/usersRouter.js";
+import db from "./db.js";
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", process.cwd() + "/views");
+// console.log(process.cwd() + "/views");
+
+// serve local files to virtual browser file system
+// app.use("/static", express.static("client"));
+
+app.use("/api/rooms", postingsRouter);
+app.use("/api/users", usersRouter);
+
+const handleListening = () => {
+  console.log(`Server listening on port http://localhost:${PORT}😀`);
+};
+
+app.listen(PORT, handleListening);
