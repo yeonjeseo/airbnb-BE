0ef6f5f82ce364f1bc118444d6b5e299bc7d637f
DESKTOP-acer <takgue1224@gmail.com>
2021-10-19 23:00:16 +0900

    reveiw

---
+++ src/controller/reviewController.js
@@ -0,0 +1,60 @@
+import Review from "../models/Review.js";
+import mongoose from "mongoose";
+
+// import { authMiddleware} from "../middlwares/Authentication";
+//get, post, patch, delete + Review 고치기
+// 변수명 전체 변경 예정
+
+
+export const getReview = async (req, res) => {
+    const { homeId ,reviewId } = req.params;
+    try {
+      const review = await Comment.find({ homId: postId }).sort("-_id");
+      res.status(200).send({ comment: comment });
+    } catch (err) {
+      res.status(400).send({ err: "코맨트 에러" })
+    }
+});
+
+// postingID => 변경
+export const postReviews = async (req, res) => {
+    const { postingId } = req.params;
+    try {
+      await Comment.find({ postingID: postingId });
+      res.sendStatus(200);
+    } catch (error) {
+      console.error(error);
+      res.sendStatus(400);
+    }
+};
+  
+export const patchReviews = async (req, res) => {
+    const { postingId } = req.params;
+    const { text } = req.body;
+    try {
+      await Comment.findOneAndUpdate(
+        { postingID: postingId, authorID: userobjectId },
+        { text }
+      );
+      res.sendStatus(200);
+    } catch (error) {
+      console.error(error);
+      res.sendStatus(400);
+    }
+};
+  
+export const deleteReviews = async (req, res) => {
+    const { postingId } = req.params;
+    try {
+      await Comment.findOneAndRemove({
+        postingID: postingId,
+        authorID: userobjectId,
+      });
+      res.sendStatus(200);
+    } catch (error) {
+      console.error(error);
+      res.sendStatus(400);
+    }
+};
+
+//아래 rating 다시
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import roomsRouter from "./routers/roomsRouter.js";
 import usersRouter from "./routers/usersRouter.js";
 import reservationRouter from "./routers/reservationRouter.js";
 import db from "./db.js";
+import reviewsRouter from "./routers/reviewsRouter.js";
 
 const app = express();
 const PORT = 4000;
@@ -18,6 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/reservation", reservationRouter);
 app.use("/api/rooms", roomsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/reviews",reviewsRouter);
 
 const handleListening = () => {
   console.log(`Server listening on port http://localhost:${PORT}😀`);
