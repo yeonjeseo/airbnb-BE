bff7a16afe60ba804303ac679a60583c948c7a79
Yeon Jeffrey <yeonjeseo@hotmail.com>
2021-10-22 15:44:58 +0900

    리뷰 라우터 수정

--- src/routers/roomsRouter.js
+++ src/routers/roomsRouter.js
@@ -1,16 +1,24 @@
 import express from "express";
-import reviewsRouter from "./reviewsRouter.js";
 import {
   postRooms,
   getOneRoom,
   getRoomsFlexible,
 } from "../controller/roomController.js";
+import {
+  postReviews,
+  patchReviews,
+  deleteReviews,
+} from "../controller/reviewController.js";
 
 const roomsRouter = express.Router();
 
 roomsRouter.route("/").get(getRoomsFlexible).post(postRooms);
 roomsRouter.route("/:roomId").get(getOneRoom);
 // roomsRouter.use("/:roomId/reviews", reviewsRouter);
-roomsRouter.route("/:roomId/reviews").get().post().delete().patch();
+roomsRouter
+  .route("/:roomId/reviews")
+  .post(postReviews)
+  .delete(deleteReviews)
+  .patch(patchReviews);
 
 export default roomsRouter;
